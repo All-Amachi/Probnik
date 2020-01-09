857073f4f8c00062bb94859b4b95f7f62bf5db98
All-Amachi <drumich44@gmail.com>
2020-01-09 17:29:43 +0200

    SAGE

---
+++ Game-master/New Unity Project/Assets/ControlScene_and_Menu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlScene_and_Menu : MonoBehaviour
+{
+    public GameObject buttonsMenu;
+    public GameObject buttonsExit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ShowExitButtons()
+    {
+        buttonsMenu.SetActive(false);
+        buttonsExit.SetActive(true);
+    }
+}
---
+++ Game-master/New Unity Project/Assets/Game/Scence/Menu/ControllerMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerMenu : MonoBehaviour
+{
+    public GameObject buttonsMenu;
+    public GameObject buttonsExit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ShowExitButtons()
+    {
+        buttonsMenu.SetActive(false);
+        buttonsExit.SetActive(true);
+    }
+    public void BackInMenu()
+    {
+        buttonsMenu.SetActive(true);
+        buttonsExit.SetActive(false);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void NewGameLoadVstyp()
+    {
+        Application.LoadLevel("Vstyp") ;
+    }
+}
+
